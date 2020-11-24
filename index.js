d8497c14ea51cf49166e4a70002d5978b3af7fe0
WaliEEE <jocularriad2@gmail.com>
2020-11-24 21:37:36 +0600

    Modified Drum

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-alert("Hello Drum Kit Website");
-
 
 //Detecting button press
-
 
 $(".drum").click(function()
 {
@@ -14,50 +11,44 @@ $(".drum").click(function()
 
 //Detecting keyboard press
 
-$(document).keypress(function(event)
+$(document).keypress((event) => 
 {
   makeSound(event.key);
   btnAnimation(event.key);
-})
+});
 
 
-function makeSound(key){
+function makeSound(key)
+{
 
   switch (key)
   {
     case 'a':
-    var aud1 = new Audio("sounds/tom-1.mp3");
-    aud1.play();
+    new Audio("sounds/tom-1.mp3").play();
     break;
 
     case 'b':
-    var aud2 = new Audio("sounds/tom-2.mp3");
-    aud2.play();
+    new Audio("sounds/tom-2.mp3").play();
     break;
 
     case 'c':
-    var aud3 = new Audio("sounds/tom-3.mp3");
-    aud3.play();
+    new Audio("sounds/tom-3.mp3").play();
     break;
 
     case 'd':
-    var aud4 = new Audio("sounds/tom-4.mp3");
-    aud4.play();
+    new Audio("sounds/tom-4.mp3").play();
     break;
 
     case 'e':
-    var aud5 = new Audio("sounds/kick-bass.mp3");
-    aud5.play();
+    new Audio("sounds/kick-bass.mp3").play();
     break;
 
     case 'f':
-    var aud6 = new Audio("sounds/snare.mp3");
-    aud6.play();
+    new Audio("sounds/snare.mp3").play();
     break;
 
     case 'g':
-    var aud7 = new Audio("sounds/crash.mp3");
-    aud7.play();
+    new Audio("sounds/crash.mp3").play();
     break;
 
     default:
@@ -72,7 +63,8 @@ function btnAnimation(currentKey)
 {
   $("."+currentKey).addClass("pressed");
 
-  setTimeout(function(){
+  setTimeout(function()
+  {
     $("."+currentKey).removeClass("pressed");
   }, 50);
 }
@@ -81,7 +73,8 @@ function rightAnimation()
 {
   $(".right").addClass("hidden");
 
-  setTimeout(function(){
+  setTimeout(function()
+  {
     $(".right").removeClass("hidden");
   }, 250);
 }
